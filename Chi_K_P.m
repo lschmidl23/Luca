b840721ae5fd13eb1c2682bc445abe27ccf9aaa5
lschmidl23 <luca.schmidlin@hsr.ch>
2017-04-05 18:27:45 +0200

    Add files via upload

--- Chi_K_P.m
+++ Chi_K_P.m
@@ -5,26 +5,29 @@ clear all; close all; clc;
 load ('GV.mat');
 load ('MK.mat');
 
+
 kvec = MK(:,7);
-pvec = 1:1:10;
+pvec = 1:1:3;
+% chivec = 0.9999:-(1/length(kvec)):0.0001;
 
 ChiKP = [];
 
 for ii = 1 : length(pvec)
-    for jj = 400:500%1 : length(kvec)
-        
-        p = pvec(ii);
-        k = kvec(jj);    
+    p = pvec(ii);
+    for jj = 1 : length(kvec)             
+        k = kvec(length(kvec)-jj);
         f = @(x) Kpfunc(x,p);
-        g = @(x) f(x) - k;                       
-        chi_zero = 0.9;
-        chi_star = fzero(g, chi_zero)   
+        g = @(x) f(x) - k;   
+%         chi_zero = chivec(jj);
+        chi_zero = 0.01;
+        chi_star = fzero(g, chi_zero);   
         ChiKP(ii,jj) = chi_star;
-        disp(ii);disp(jj);
+        disp(ii),disp(jj),disp(chi_star);
     end
 end
 
 disp (ChiKP);
+
 
 
 % x = linspace(0,1);
@@ -34,3 +37,14 @@ disp (ChiKP);
 %                 ylabel('k')
 %                 title(sprintf('p = %f', p))
 %                 clear x
+
+% p = 1;
+% x = linspace(-10,10);
+% 
+% 
+% plot(x,Kpfunc(x,p))
+% set(gca, 'YScale', 'log');
+% grid on
+% xlabel('\chi')
+% ylabel('k')
+% title(sprintf('p = %f', p))
